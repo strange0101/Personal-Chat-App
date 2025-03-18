bfcdc6378357ca2d41dc58bda4f96d7d8c704aa0
Aryan Sahu <121814870+strange0101@users.noreply.github.com>
2025-03-18 15:19:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN],
+    origin: ["https://friendly-paprenjak-58936d.netlify.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
